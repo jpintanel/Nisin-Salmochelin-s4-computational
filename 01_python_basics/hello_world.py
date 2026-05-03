85f81d8cd67f54b9a8c7ec65eb55adcee9ced02f
jpintanel <josepintanel7@gmail.com>
2026-05-03 11:53:17 -0300

    First Python script

---
+++ 01_python_basics/hello_world.py
@@ -0,0 +1,6 @@
+# My first script — Nisin-Salmochelin S4 project
+# Jose Pintanel — PhD UNT Argentina
+print("Hello, World!")
+print("Hello, GitHub!")
+print("This is the beginning of the computational analysis")
+print("of the Nisin - Salmochelin S4 conjugate.")
